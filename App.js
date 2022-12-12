78e807010ea3167057f315fb301a2a7dde2e1b88
saifulislam05 <saiful.bijulia@gmail.com>
2022-12-12 07:30:51 +0530

    scroll view and list

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+// import { useState } from 'react';
+import { useState } from 'react';
+import { StyleSheet, Text, View , TextInput} from 'react-native';
+import { ScrollView } from 'react-native';
+import { RefreshControl } from 'react-native';
+
+
+
 
 export default function App() {
+  const [items, setItems] = useState([
+  {key:1,item:"item 1"},
+  {key:2,item:"item 2"},
+  {key:3,item:"item 3"},
+  {key:4,item:"item 4"},
+  {key:5,item:"item 5"},
+  {key:6,item:"item 6"},
+
+])
+const [refreshing, setRefreshing]=useState(false)
+  const onRefresh = () => {
+    setRefreshing(true)
+    setItems([...items])
+  }
+  
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <ScrollView
+        refreshControl={<RefreshControl refreshing={refreshing} />}
+        style={styles.body}
+      >
+        {items.map((item) => (
+          <View style={styles.item} key={item.key}>
+            <Text style={styles.text}>{item.item}</Text>
+          </View>
+        ))}
+      </ScrollView>
+      <TextInput
+        style={styles.input}
+        // onChangeText={onChangeText}
+        value="type something here"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: 30,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  item: {
+    backgroundColor: "#4ae1fa",
+    marginTop: 20,
+  },
+  text: {
+    color: "#00000",
+    fontSize: 35,
+    fontStyle: "italic",
+    margin: 10,
   },
 });
